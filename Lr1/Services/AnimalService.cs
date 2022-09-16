65d6123f19503d78bec58c357860be7ab5dfeaae
Константи Гузик <konstantinguzik7@gmail.com>
2022-09-16 11:43:09 +0300

    .

---
+++ Lr1/Services/AnimalService.cs
@@ -0,0 +1,27 @@
+﻿using Lr1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Lr1.Services
+{
+    public class AnimalService
+    {
+        List<Animal> animalList = new();
+        public async Task<IEnumerable<Animal>> GetFood()
+        {
+            if (animalList?.Count > 0)
+                return animalList;
+            using var stream = await FileSystem.OpenAppPackageFileAsync("Animal.json");
+            using var reader = new StreamReader(stream);
+            var contents = await reader.ReadToEndAsync();
+            animalList = JsonSerializer.Deserialize<List<Animal>>(contents);
+
+            return animalList;
+
+        }
+    }
+}
